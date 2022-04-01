6e0a6d77d7a12cf1d87ccd28fd98d19ee5d72cea
Saksham Ayush <saksham.barca@gmail.com>
2022-03-31 20:40:43 -0500

    init java file added

---
+++ src/main/java/init/dao/InitDao.java
@@ -0,0 +1,49 @@
+package init.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import user.domain.User;
+
+
+
+/**
+ * DDL functions performed in database
+ */
+public class InitDao {
+	
+	/**
+	 * user name to connect to the database 
+	 */
+	private String MySQL_user = "JSD";  //TODO change user
+	
+	/**
+	 * password of your username to connect to the database
+	 */
+	private String MySQL_password = "jega2rox";  //TODO change password
+	
+	
+
+	public void initlaize() throws ClassNotFoundException {
+		// TODO Auto-generated method stub
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection connect = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/bookstore", MySQL_user, MySQL_password);
+			
+			String sql = "create table test (id int)";
+			PreparedStatement preparestatement = connect.prepareStatement(sql); 
+		    preparestatement.executeUpdate();
+		    connect.close();
+		} catch(SQLException e) {
+			throw new RuntimeException(e);
+		}
+		
+	}
+		
+}
