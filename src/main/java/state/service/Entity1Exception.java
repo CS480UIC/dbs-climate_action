b65be286b989585020d8e49514e6a60589506272
Saksham Ayush <saksham.barca@gmail.com>
2022-04-27 16:22:43 -0500

    Addeasdadada minor changes

---
+++ src/main/java/state/service/Entity1Exception.java
@@ -0,0 +1,18 @@
+package state.service;
+
+public class Entity1Exception extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public Entity1Exception() {
+		super();
+	}
+
+	public Entity1Exception(String message) {
+		super(message);
+	}
+
+}
