abe017d2cf3d866a7c5ff088ebedcbc078325fec
Saksham Ayush <42735975+SakshamAyush@users.noreply.github.com>
2022-05-04 01:29:53 -0500

    Update initializeDB.sql

--- sql/initializeDB.sql
+++ sql/initializeDB.sql
@@ -57,6 +57,7 @@ CREATE TABLE `carbon_dioxide` (
   `emission_year` int NOT NULL,
   `co_metric` decimal(6,2) NOT NULL,
   PRIMARY KEY (`co_id`,`emission_year`),
+  UNIQUE KEY (`co_id` (`co_id`), `emission_year` (`emission_year`)),
   CONSTRAINT `carbon_dioxide_ibfk_1` FOREIGN KEY (`co_id`) REFERENCES `state` (`code`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
